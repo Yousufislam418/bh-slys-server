0eb7bfeadf1a1c9cd84438edc62346d382100755
Yousufislam418 <yousufislamaps@gmail.com>
2025-08-08 02:39:57 +0300

    everything

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require('mongoose');
 const port = 3003;
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 app.use(cors());
 app.use(express.json());
@@ -13,6 +15,19 @@ const User = require('./models/users');
 app.get('/', async(req,res)=> {
     res.send('bh-slys-server is online');
 });
+
+//----------------Token Verify------------------------------------------->
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Access Denied" });
+  try {
+    const verified = jwt.verify(token, process.env.SECRET_TOKEN);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(403).json({ message: "Invalid Token" });
+  }
+};
 
 // MongoDB Connect
 mongoose.connect(process.env.MONGODB_URL, {
@@ -43,47 +58,93 @@ app.put('/visitors/:id', async(req,res)=> {
 });
 
 //----------------------------------> users start
-// POST : users data
-app.post('/users', async(req,res)=> {
+// Register
+app.post('/register', async(req,res)=>{
+  const userDatas = req.body;
+  const {email,password} = userDatas;
+  const existingUser = await User.findOne({email});
+ const hashPassword = await bcrypt.hash(password, 10);
  try{
-  const {name,email,number,password,location,age,image,date} = req.body;
-  const newUser = new User({name,email,number,password,location,age,image,date});
-  const result = await newUser.save();
-  res.status(201).json(result).send(result);
- }catch(error){
-  res.status(500).json({error: 'Unsuccessfully'});
+   if(existingUser){
+    return res.status(400).json({error: 'User already create'});
+   }else{
+    const users = new User({...userDatas, password: hashPassword});
+    const result = await users.save();  
+    res.status(201).json(result);
+   }
+ }catch(err){
+   res.status(403).json({error: err});
  }
 });
-// READ : GET all users
-app.get('/users', async(req,res)=> {
- try{
-  const users = await User.find();
-  res.json(users).send(users);
- }catch(error){
-  res.status(500).json({error: 'User get failed'});
+
+// Login
+app.post('/login', async(req,res)=>{
+  const {email,password} = req.body;
+  const user = await User.findOne({email});
+  if(!user || !(await bcrypt.compare(password, user.password))){
+    return res.status(401).json({error: 'invalid credentials'});
+  }else{
+    const token = jwt.sign({userId: user._id}, process.env.SECRET_TOKEN, {expiresIn: "30d"});
+    res.status(200).json({token}); 
  }
 });
-// READ : GET one user
-app.get('/users/:id', async(req,res)=> {
+
+// profile
+app.get('/profile', verifyToken, async(req,res)=>{
  try{
-  const user = await User.findById(req.params.id);
-  if(!user) return res.status(404).send('User not found');
-  res.json(user).send(user);
- }catch(error){
-  res.status(500).send(error);
+  const user = await User.findById(req.user.userId).select('-password');
+  res.json(user);
+ }catch(err){
+   res.status(403).json({error: 'Invalid token'});
  }
 });
+
+// Get All Users : Match Token
+app.get('/users',verifyToken, async(req,res)=>{
+ try{
+    const user = await User.find().select('-password');
+  res.json(user);
+ }catch(err){
+   res.status(403).json({error: 'Invalid token'});
+ }
+}); 
+
+// Get one user : using id
+app.get('/users/:id',verifyToken, async(req,res)=>{
+ try{
+    const user = await User.findById(req.params.id);
+  res.json(user);
+ }catch(err){
+   res.status(403).json({error: 'Invalid token'});
+ }
+});
+
 // UPDATE : PUT user update
 app.put('/users/:id', async(req,res)=> {
  try{
   const id = req.params.id;
-  const datas = req.body;
+  const datas = req.body;   
   const result = await User.findByIdAndUpdate(id, datas, {new: true});
   res.json(result).send(result); 
  }catch(error){
   res.status(500).send(error);
  }
 });
+
+// UPDATE PASSWORD : PUT user password update
+app.put('/users-pass/:id', async(req,res)=> {
+ try{
+  const id = req.params.id;
+  const datas = req.body;
+  const newPass = datas.password;
+  const hashPassword = await bcrypt.hash(newPass, 10);
+  const result = await User.findByIdAndUpdate(id, {password:hashPassword}, {new: true});
+  res.json(result); 
+ }catch(error){
+  res.status(500).send(error);
+ }
+});
+
 // DELETE : one user delete
 app.delete('/users/:id', async(req,res)=> {
  try{
